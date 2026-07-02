581ddf2f47d643192d59e3a371558446df027986
Nalin Narayan <160528770+NalNar@users.noreply.github.com>
2026-07-02 17:54:34 +0000

    update

--- projects/ToDo/my-react-app/vite.config.js
+++ projects/ToDo/my-react-app/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/projects/ToDo/my-react-app/dist/',
+  base: '/projects/ToDo/my-react-app/',
 })
